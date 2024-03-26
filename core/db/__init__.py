1d67a68956ffdeaffa81004cdbaf2422a10912e1
Kabir Maniar <kabir.maniar@maniartech.com>
2024-03-26 15:09:10 +0530

    Added Redis Packages and Initialisations in the code

--- core/db/__init__.py
+++ core/db/__init__.py
@@ -2,9 +2,14 @@
 # from sqlalchemy.orm import DeclarativeBase, Session
 # from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
+from redis import Redis
+
 
 db = SQLAlchemy()
 test_db = SQLAlchemy()
+
+redis = Redis(host='localhost', port=6379,decode_responses=True)
+
 def get_db():
     """
     Returns the database object.
@@ -21,3 +26,7 @@ def get_session():
 def get_test_session():
     """Returns a session object bound to the test engine"""
     return test_db.session
+
+def get_redis():
+    """Returns the redis object"""
+    return redis
